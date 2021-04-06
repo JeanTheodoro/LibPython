ae5e8456b633585dbafc7057da9b543ab05a8192
Jean Theodoro <69826260+JeanTheodoro@users.noreply.github.com>
2021-04-06 20:54:32 -0300

    Branch/test database (#9)
    * Emulated test in thr database
    
    * Scope Fixture
    
    * File Conftest
    
    * Testable test production
    
    * mock

--- libpython/spam/send_email.py
+++ libpython/spam/send_email.py
@@ -1,10 +1,8 @@
 class SendEmail:
-    qtd_email_send = 0
 
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email de rementente inválido {remetente}')
-        self.qtd_email_send += 1
         return remetente
 
 
